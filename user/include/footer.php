41d2f8a8e73415a8cbed36ce4dac20e201b0f00b
JorielPablo <145237999+JorielPablo@users.noreply.github.com>
2023-09-17 09:38:18 +0800

    Add files via upload

---
+++ user/include/footer.php
@@ -0,0 +1,12 @@
+      <footer class="py-4 bg-dark text-white-50 fixed-bottom">
+        <div class="container text-center">
+                 </div>
+      </footer>
+
+      <!-- Bootstrap core JavaScript-->
+      <script src="../vendor/jquery-3.4.1.min.js"></script>
+      <script src="../vendor/bootstrap/popper.min.js"></script>
+      <script src="../vendor/bootstrap/bootstrap.min.js"></script>
+  </body>
+
+</html>
---
+++ user/index.php
@@ -0,0 +1,43 @@
+<?php include('include/header.php'); ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Smart | Agriculture - User</title>
+  <!-- favicon -->
+  <link rel="shortcut icon" type="image/x-icon" href="../images/logo - Copy.png">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+
+    <!-- Bootstrap v4.4.1 -->
+    <link rel="stylesheet" type="text/css" href="../vendor/bootstrap/bootstrap.min.css">
+
+    <!-- Font Awesome Free 5.12.0 -->
+    <link rel="stylesheet" type="text/css" href="../vendor/fontawesome/css/all.css">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="../images/favicon.jpg">
+<!-- bootstrap 5 css -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css" integrity="sha384-DhY6onE6f3zzKbjUPRc2hOzGAdEf4/Dz+WJwBvEYL/lkkIsI3ihufq9hk9K4lVoK" crossorigin="anonymous">
+    <!-- custom css -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+    <body class="bg-light">
+      
+      <!-- Navigation -->
+      
+  <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Welcome!</h1>
+        <p class="lead text-primary ont-weight-bold text-uppercase"><?php echo $_SESSION['fullname']; ?></p>
+        <hr class="my-4">
+      </div>
+  </div>
+
+<?php include('include/footer.php'); ?>
